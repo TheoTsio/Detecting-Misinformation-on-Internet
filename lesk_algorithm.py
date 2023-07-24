8d2f0c3cbdb1210a0ad7c2c4978ae1ade333dab3
Theodoros Tsiolakis <77570166+TheoTsio@users.noreply.github.com>
2023-07-24 17:39:11 +0300

    Update lesk_algorithm.py

--- lesk_algorithm.py
+++ lesk_algorithm.py
@@ -1,7 +1,1 @@
-def lesk_algorithm(sentence):
-    query = word_tokenize(sentence)
-    apply_lesk = lesk_algorithm_unit(query, "vitiligo")
-    if apply_lesk:
-        print([lemma.name() for lemma in apply_lesk.lemmas()])
-    else:
-        print([])
+
